bf9c7af3e5fd49d000cf19f6a45e9deb4fea0c0d
CoherenceController <qlab@bbn.com>
2014-05-05 13:45:55 -0400

    Cooling Log Program using CryoCon
    First push, written for monitoring experiment in 3K Dry fridge
    -KC

---
+++ Expt Control/3K Dry Fridge/CryoCon_CoolLog.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%     What and hOw?      %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Cooling log using CryoCon Temperature Controller
+% Created in May 2014 by Jesse Crossno and KC Fong
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%     CLEAR  and INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+temp = instrfind;
+if ~isempty(temp)
+    fclose(temp)
+    delete(temp)
+end
+clear temp StartTime start_dir CoolLogData;
+close all;
+fclose all;
+
+% Connect to the Cryo-Con 22 temperature controler
+TC = deviceDrivers.CryoCon22();
+TC.connect('12');
+
+% Initialize variables
+DataInterval = input('Time interval in temperature readout (in second) = ');
+start_dir = 'C:\Users\qlab\Documents\Graphene Data\';
+start_dir = uigetdir(start_dir);
+StartTime = clock;
+FileName = strcat('CoolLog_', datestr(StartTime, 'yyyymmdd_HHMMSS'), '.dat');
+FilePtr = fopen(fullfile(start_dir, FileName), 'w');
+fprintf(FilePtr, strcat(datestr(StartTime), ' CoolLog using CryoCon\r\n'));
+fprintf(FilePtr,'Time_s\tTemperature_K\r\n');
+fclose(FilePtr);
+
+% temperature log loop
+j=1;
+figure;
+while true
+    CoolLogData(j,:) = [etime(clock, StartTime) TC.temperatureA()];
+    pause(DataInterval);
+    FilePtr = fopen(fullfile(start_dir, FileName), 'a');
+    fprintf(FilePtr,'%d\t%G\r\n',CoolLogData(j,:));
+    fclose(FilePtr);
+    plot(CoolLogData(:,1)/60, CoolLogData(:,2)); grid on; xlabel('Time (min.)'); ylabel('Temperature (K)'); title(strcat('CoolLog using CryoCon, start date and time: ', datestr(StartTime)));
+    j = j+1;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%       Clear     %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+TC.disconnect();
+clear TC;

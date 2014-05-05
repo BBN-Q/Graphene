6fb81267a235cda6add1fc7fc59da954a5892735
CoherenceController <qlab@bbn.com>
2014-05-05 19:52:43 -0400

    Cross-correlation noise thermometry experiments
    Experiments to understand the performance of the cross-correlation noise
    thermometry by varying 3K Dry Fridge temperature.
    - KC

---
+++ Expt Control/3K Dry Fridge/XCNoise_vs_T_Auto_v1.m
@@ -0,0 +1,58 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%     What and hOw?      %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Manual Cross-Correlation Noise Testing
+% version 1.0
+% Created in May 2014 by KC Fong
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%     CLEAR  and INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function XCNoiseData = XCNoise_vs_T_Auto_v1(SetTArray)
+temp = instrfind;
+if ~isempty(temp)
+    fclose(temp)
+    delete(temp)
+end
+clear temp StartTime start_dir XCNoiseData j;
+close all;
+fclose all;
+
+% Connect to the Cryo-Con 22 temperature controler
+TC = deviceDrivers.CryoCon22();
+TC.connect('12');
+DVM = deviceDrivers.Keithley197();
+DVM.connect('19');
+
+% Initialize variables
+start_dir = 'C:\Users\qlab\Documents\Graphene Data\';
+start_dir = uigetdir(start_dir);
+StartTime = clock;
+FileName = strcat('XCNoise_', datestr(StartTime, 'yyyymmdd_HHMMSS'), '.dat');
+FilePtr = fopen(fullfile(start_dir, FileName), 'w');
+fprintf(FilePtr, strcat(datestr(StartTime), ' Cross Correlation Noise vs. Temperature using CryoCon\r\n'));
+fprintf(FilePtr,'CryoConT_K\tMultiplier_V\r\n');
+fclose(FilePtr);
+
+% temperature log loop
+j=1; WaitTime = 60*5;
+figure;
+for m = 2:length(SetTArray)
+    FilePtr = fopen(fullfile(start_dir, FileName), 'a');
+    for k=1:20
+        XCNoiseData(j,:) = [TC.temperatureA() str2num(DVM.value())]
+        fprintf(FilePtr,'%f\t%f\r\n',XCNoiseData(j,:));
+        j = j+1;
+        pause(3);
+    end    
+    fclose(FilePtr);
+    TC.loopTemperature = SetTArray(m);
+    plot(XCNoiseData(:,1), XCNoiseData(:,2)); grid on; xlabel('T_{CryoCon} (K)'); ylabel('V_{Cross-Correlation} (V)'); title(strcat('XC Noise ', pwd));
+    pause(WaitTime);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%       Clear     %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+TC.disconnect(); DVM.disconnect();
+clear TC DVM;

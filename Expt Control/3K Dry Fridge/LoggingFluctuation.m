5337013985a26cfad6a39cee4306a35d9b1e67da
CoherenceController <qlab@bbn.com>
2014-05-09 18:18:34 -0400

    Weekly updates

--- Expt Control/3K Dry Fridge/LoggingFluctuation.m
+++ Expt Control/3K Dry Fridge/LoggingFluctuation.m
@@ -1,52 +1,55 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%     What and hOw?      %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% One quantity monitoring program
+% Cooling log using CryoCon Temperature Controller
 % Created in May 2014 by Jesse Crossno and KC Fong
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%     CLEAR  and INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function LogData = Monitor()
+function CoolLogData = LoggingFluctuation()
 temp = instrfind;
 if ~isempty(temp)
     fclose(temp)
     delete(temp)
 end
-clear temp StartTime start_dir LogData;
+clear temp StartTime start_dir CoolLogData;
 close all;
 fclose all;
 
 % Connect to the Cryo-Con 22 temperature controler
+TC = deviceDrivers.CryoCon22();
 DVM = deviceDrivers.Keithley197();
+
 
 % Initialize variables
 DataInterval = input('Time interval in temperature readout (in second) = ');
-start_dir = pwd;
+start_dir = 'C:\Users\qlab\Documents\Graphene Data\3K Fridge CoolLog Repository\';
 start_dir = uigetdir(start_dir)
 StartTime = clock;
-FileName = strcat('DataLog_', datestr(StartTime, 'yyyymmdd_HHMMSS'), '.dat');
+FileName = strcat('LogFluctuation_', datestr(StartTime, 'yyyymmdd_HHMMSS'), '.dat');
 FilePtr = fopen(fullfile(start_dir, FileName), 'w');
-fprintf(FilePtr, strcat(datestr(StartTime), 'Monitoring program\r\n'));
-fprintf(FilePtr,'Time_s\tTemperature_K\r\n');
+fprintf(FilePtr, strcat(datestr(StartTime), ' CoolLog using CryoCon\r\n'));
+fprintf(FilePtr,'Time_s\tTemperature_K\tMultiplierV_V\r\n');
 fclose(FilePtr);
 
 % temperature log loop
 j=1;
 figure;
 while true
-    DVM.connect('19');
-    LogData(j,:) = [etime(clock, StartTime) str2num(DVM.value())];
-    DVM.disconnect();
+    TC.connect('12'); DVM.connect('19');
+    CoolLogData(j,:) = [etime(clock, StartTime) TC.temperatureA() str2num(DVM.value())];
+    TC.disconnect(); DVM.disconnect();
     pause(DataInterval);
     FilePtr = fopen(fullfile(start_dir, FileName), 'a');
-    fprintf(FilePtr,'%0.3f\t%f\r\n',LogData(j,:));
+    fprintf(FilePtr,'%0.3f\t%f\t%f\r\n',CoolLogData(j,:));
     fclose(FilePtr);
-    plot(LogData(:,1)/60, 1000*LogData(:,2)); grid on; xlabel('Time (min.)'); ylabel('DVM (mV)'); title(strcat('Data Log, start date and time: ', datestr(StartTime)));
+    plotyy(CoolLogData(:,1)/60, CoolLogData(:,2), CoolLogData(:,1)/60, CoolLogData(:,3)); grid on; xlabel('Time (min.)'); ylabel('Temperature (K)'); title(strcat('Tracking fluctuation using CryoCon and DVM, start date and time: ', datestr(StartTime)));
     j = j+1;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%       Clear     %%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear DVM;
+
+clear TC;
--- Expt Control/3K Dry Fridge/XCNoise_vs_T_Auto_v1.m
+++ Expt Control/3K Dry Fridge/XCNoise_vs_T_Auto_v1.m
@@ -8,7 +8,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%     CLEAR  and INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function XCNoiseData = XCNoise_vs_T_Auto_v1(SetTArray)
+function XCNoiseData = XCNoise_vs_T_Auto_v1(SetTArray, inst)
 temp = instrfind;
 if ~isempty(temp)
     fclose(temp)
@@ -20,11 +20,19 @@ fclose all;
 
 % Connect to the Cryo-Con 22 temperature controler
 TC = deviceDrivers.CryoCon22();
-TC.connect('12');
-DVM = deviceDrivers.Keithley197();
-DVM.connect('19');
+switch inst
+    case 'DVM'
+        DVM = deviceDrivers.Keithley197();
+    case 'lockin'
+        Lockin = deviceDrivers.SRS830();
+    otherwise
+        warning('Instruments to measure cross-correlation voltage: DVM or lockin');
+        SetTArray = [];
+end
 
 % Initialize variables
+TWaitTime = input('Enter waiting time for temperature stabilizing to new set point in seconds: ');
+LockinWaitTime = input('Enter waiting time for new cross-correlation voltage data point in seconds: ');
 start_dir = 'C:\Users\qlab\Documents\Graphene Data\';
 start_dir = uigetdir(start_dir);
 StartTime = clock;
@@ -35,33 +43,51 @@ fprintf(FilePtr,'CryoConT_K\tMultiplier_V\r\n');
 fclose(FilePtr);
 
 % temperature log loop
-j=1; WaitTime = 60*5;
+j=1;
 figure; pause on; %pause(WaitTime*1.5);
 for m = 1:length(SetTArray)
     FilePtr = fopen(fullfile(start_dir, FileName), 'a');
+    TC.connect('12');
     for k=1:20
-        XCNoiseData(j,:) = [TC.temperatureA() str2num(DVM.value())]
-        fprintf(FilePtr,'%f\t%f\r\n',XCNoiseData(j,:));
+        switch inst
+            case 'DVM'
+                DVM.connect('19');
+                XCNoiseData(j,:) = [TC.temperatureA() str2num(DVM.value())]
+                fprintf(FilePtr,'%f\t%e\r\n', XCNoiseData(j,:));
+                DVM.disconnect();
+            case 'lockin'
+                Lockin.connect('8');
+                XCNoiseData(j,:) = [TC.temperatureA() Lockin.X Lockin.Y]
+                fprintf(FilePtr,'%f\t%e\t%e\r\n', XCNoiseData(j,:));
+                Lockin.disconnect();
+            otherwise
+                XCNoiseData(j,:) = [TC.temperatureA() 0]
+                fprintf(FilePtr,'%f\t%e\r\n', XCNoiseData(j,:));
+        end
         j = j+1;
-        pause(1.5);
+        pause(LockinWaitTime);
     end    
     fclose(FilePtr);
-    TC.loopTemperature = SetTArray(m);
-    if SetTArray(m) < 21
-        TC.range='MID'; TC.pGain=1; TC.iGain=10;
-    elseif SetTArray(m) < 30
-        TC.range='MID'; TC.pGain=10; TC.iGain=70;
-    elseif SetTArray(m) < 45
-        TC.range='MID'; TC.pGain=50; TC.iGain=70;
-    else SetTArray(m) < 80
-        TC.range='HI'; TC.pGain=50; TC.iGain=70;
-    end           
+    if m < length(SetTArray)
+        TC.loopTemperature = SetTArray(m+1);
+        if SetTArray(m) < 21
+            TC.range='MID'; TC.pGain=1; TC.iGain=10;
+        elseif SetTArray(m) < 30
+            TC.range='MID'; TC.pGain=10; TC.iGain=70;
+        elseif SetTArray(m) < 45
+            TC.range='MID'; TC.pGain=50; TC.iGain=70;
+        else
+            TC.range='HI'; TC.pGain=50; TC.iGain=70;
+        end
+    else
+        TC.loopTemperature = 0.001; TC.range='LOW'; TC.pGain=1; TC.iGain=1;
+    end
+    TC.disconnect();
     plot(XCNoiseData(:,1), XCNoiseData(:,2)); grid on; xlabel('T_{CryoCon} (K)'); ylabel('V_{Cross-Correlation} (V)'); title(strcat('XC Noise ', pwd));
-    pause(WaitTime);
+    pause(TWaitTime);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%       Clear     %%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-TC.disconnect(); DVM.disconnect();
-clear TC DVM;
+clear TC;

91b59edbf08c9dcae55e6bc49fa242fdcd03c251
CoherenceController <qlab@bbn.com>
2014-05-29 08:46:55 -0400

    Testing of using Alazar Tech in Cross-Correlation Measurements

---
+++ Expt Control/3K Dry Fridge/XCNoise_vs_T_Auto_v2.m
@@ -0,0 +1,76 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%     What and hOw?      %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Manual Cross-Correlation Noise Testing
+% version 2.0
+% Created in May 2014 by KC Fong
+% Using ALAZAR TECH
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%     CLEAR  and INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [XCNoiseData, XCNoiseStatistics] = XCNoise_vs_T_Auto_v2(SetTArray)
+% Connect to the Cryo-Con 22 temperature controler
+TC = deviceDrivers.CryoCon22();
+
+% Initialize variables
+TWaitTime = input('Enter waiting time for temperature stabilizing to new set point in seconds: ');
+start_dir = pwd;
+start_dir = uigetdir(start_dir);
+StartTime = clock;
+FileName = strcat('XCNoise_', datestr(StartTime, 'yyyymmdd_HHMMSS'), '.dat');
+FilePtr = fopen(fullfile(start_dir, FileName), 'w');
+fprintf(FilePtr, strcat(datestr(StartTime), ' Cross Correlation Noise vs. Temperature using CryoCon\r\n'));
+fprintf(FilePtr,'CryoConT_K\tCrossCorrelatedV_V\r\n');
+fclose(FilePtr);
+
+% temperature log loop
+j=1;
+figure; pause on; %pause(WaitTime*1.5);
+for m = 1:length(SetTArray)
+    FilePtr = fopen(fullfile(start_dir, FileName), 'a');
+    TC.connect('12');
+    sprintf(strcat('Taking data at set T = ', num2str(SetTArray(m)), ', progress = ', num2str(100*m/length(SetTArray)), '%%'))
+    for k=1:2
+        DoubleTraces = GetAlazarTraces(0.04, 100e6, 160240000, 'False');
+        XCNoiseStatistics.ChAMean(j) = mean(DoubleTraces(:,2));
+        XCNoiseStatistics.ChBMean(j) = mean(DoubleTraces(:,3));
+        XCNoiseStatistics.ChAStd(j) = std(DoubleTraces(:,2));
+        XCNoiseStatistics.ChBStd(j) = std(DoubleTraces(:,3));
+        XCNoiseStatistics.Temperature(j) = TC.temperatureA();
+        DoubleTraces(:,2) = DoubleTraces(:,2) - XCNoiseStatistics.ChAMean(j);
+        DoubleTraces(:,3) = DoubleTraces(:,3) - XCNoiseStatistics.ChBMean(j);
+        XCNoiseData(j,:) = [XCNoiseStatistics.Temperature(j) dot(DoubleTraces(:,2), DoubleTraces(:,3))/length(DoubleTraces)];
+        fprintf(FilePtr,'%f\t%e\r\n', XCNoiseData(j,:));
+        j = j+1;
+    end    
+    fclose(FilePtr);
+    %AllDoubleTraces(:,j) = DoubleTraces(:,2);
+    if m < length(SetTArray)
+        TC.loopTemperature = SetTArray(m+1);
+        if SetTArray(m) < 21
+            TC.range='MID'; TC.pGain=1; TC.iGain=10;
+        elseif SetTArray(m) < 30
+            TC.range='MID'; TC.pGain=10; TC.iGain=70;
+        elseif SetTArray(m) < 45
+            TC.range='MID'; TC.pGain=50; TC.iGain=70;
+        elseif SetTArray(m) < 100
+            TC.range='HI'; TC.pGain=50; TC.iGain=70;
+        else
+            TC.range='HI'; TC.pGain=50; TC.iGain=70;
+        end
+    else
+        TC.loopTemperature = 0.001; TC.range='LOW'; TC.pGain=1; TC.iGain=1;
+    end
+    TC.disconnect();
+    plot(XCNoiseData(:,1), XCNoiseData(:,2)); grid on; xlabel('T_{CryoCon} (K)'); ylabel('V_{xc} (V)'); title(strcat('XC Noise ', pwd));
+    if m < length(SetTArray)
+        sprintf(strcat('Waiting to new set T = ', num2str(SetTArray(m+1)), '...'))
+        pause(TWaitTime);
+    end
+end
+pause off;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%       Clear     %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear TC;
--- Expt Control/Common/GetAlazarTraces.m
+++ Expt Control/Common/GetAlazarTraces.m
@@ -1,7 +1,8 @@
 function AlazarTraces = GetAlazarTraces(PlusMinusVoltage, SampleRate, SampleLength, plotFlag)
+StartTime = clock;
 scope = deviceDrivers.AlazarATS9870();
 scope.connect(0)
-scope.vertical = struct('verticalScale', PlusMinusVoltage, 'verticalCoupling', 'DC', 'bandwidth', 'Full');
+scope.vertical = struct('verticalScale', PlusMinusVoltage, 'verticalCoupling', 'AC', 'bandwidth', 'Full');
 scope.horizontal = struct('samplingRate', SampleRate, 'delayTime', 0);
 scope.trigger = struct('triggerLevel', 100, 'triggerSource', 'ext', 'triggerCoupling', 'DC', 'triggerSlope', 'rising');
 scope.acquireStream(SampleLength);
@@ -11,3 +12,5 @@ AlazarTraces(:,2) = scope.data{1}; AlazarTraces(:,3) = scope.data{2};
 if strcmp(plotFlag, 'True')
     figure(); plot(AlazarTraces(:,1), AlazarTraces(:,2)); hold on; plot(AlazarTraces(:,1), AlazarTraces(:,3), 'r');
 end
+clear scope;
+disp(num2str(etime(clock, StartTime)));

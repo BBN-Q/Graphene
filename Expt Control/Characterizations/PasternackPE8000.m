b47061a35baf3c5efd5fddb2967dac5128c416ff
CoherenceController <qlab@bbn.com>
2015-04-03 16:35:18 -0400

    PasternackPE8000 Characterization
    Checking Power diode linearity.
    KC

---
+++ Expt Control/Characterizations/PasternackPE8000.m
@@ -0,0 +1,69 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%     CLEAR      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+temp = instrfind;
+if ~isempty(temp)
+    fclose(temp)
+    delete(temp)
+end
+% clear temp sigGen spec
+% close all
+% fclose all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%     INITIALIZE PATH     %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+base_path = 'C:\Users\qlab\Documents\data\Graphene data\PE8000 Characterization\';
+cd(base_path)
+% addpath([ base_path,'data'],'-END');
+filename = 'PasternackPE8000_10MHz_v1.txt';
+data = fopen(filename,'w');
+pause on;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%     INITIALIZE  EXPERIMENT      %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+pauseTime = 1;
+
+% Connect to instruments
+sigGen = deviceDrivers.AgilentN5183A();
+sigGen.connect('128.33.89.196');
+DVM = deviceDrivers.Keithley197();
+DVM.connect('16')
+%%%%%%%%%
+%run information
+startPower = -20; % in dBm
+endPower = 20; % in dBm
+PowerArray = startPower:1:endPower;
+pauseTime = 2;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%     RUN THE EXPERIMENT      %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SigGenPower = startPower;
+for j=1:length(PowerArray)
+    sigGen.power = PowerArray(j);
+    pause(pauseTime);
+    Result = DVM.get();
+    PowerArray_W(j) = 0.001*10^(PowerArray(j)*0.1);
+    Vdiode(j) = str2num(Result.value);
+    fprintf(data,'%d %e %e \r\n', PowerArray(j), PowerArray_W(j), Vdiode(j));
+end
+sigGen.power = -20;
+figure; plot(PowerArray_W, Vdiode, 'd'); grid on;
+        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%       PLOT AND SAVE DATA     %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+pause off ;
+sigGen.disconnect();
+DVM.disconnect();
+fclose(data);
+
+

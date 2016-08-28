d74cb90e1d45f4484ebc2273e1ebef96becde011
KC Fong <kc.fong@bbn.com>
2016-08-28 10:21:05 -0400

    Microwave scanning programs

--- Analysis/Common/GetIndex.m
+++ Analysis/Common/GetIndex.m
@@ -2,7 +2,13 @@
 % not using interp1() because it demands a monotonic function
 
 function result = GetIndex(XArray, XValue)
-
-    [minValue, result] = min(abs(XArray-XValue));
-
+    ArraySize = size(XValue);
+    if max(ArraySize)~= 1
+        for k = 1:max(ArraySize)
+            [minValue, minIndex] = min(abs(XArray-XValue(k)));
+            result(k) = minIndex;
+        end
+    else
+        [minValue, result] = min(abs(XArray-XValue));
+    end
 end

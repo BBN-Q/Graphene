1463ad5eb99b35f927a0da418f5583d0afc2ae26
KC Fong <kc.fong@bbn.com>
2016-09-20 00:59:59 -0400

    New program and fixes

--- Analysis/JJ Analysis/JJdcSweep4Ic.m
+++ Analysis/JJ Analysis/JJdcSweep4Ic.m
@@ -1,6 +1,6 @@
 % Getting Ic and Ir from dcV of the JJ acquired from NI-DAQ
 
-function result = JJdcSweep4Ic(data, TrimRatio)
+function result = JJdcSweep4Ic(data, TrimRatio, SmoothSpan)
 dClock = diff(data.clocking);
 OneSweepLength = floor(data.SweepTime*data.fs);
 IBias = linspace(data.IbiasRange(2), data.IbiasRange(1), OneSweepLength);
@@ -8,9 +8,10 @@ StartingIndex = 1;
 iCounter = 1;
 for k = 1:length(dClock)
     if dClock(k) > 2.5
-        CriticalCurrent = GetCriticalCurrent(IBias(1+floor(TrimRatio*OneSweepLength):floor(end/2)), data.dcV(StartingIndex+floor(TrimRatio*OneSweepLength):StartingIndex-1+floor(OneSweepLength/2)));
+        % MovingAvg is a function to replace smooth moving avg
+        CriticalCurrent = GetCriticalCurrent(IBias(1+floor(TrimRatio*OneSweepLength):floor(end/2)), data.dcV(StartingIndex+floor(TrimRatio*OneSweepLength):StartingIndex-1+floor(OneSweepLength/2)), SmoothSpan);
         result.Ir(iCounter) = CriticalCurrent.DiffMin;
-        CriticalCurrent = GetCriticalCurrent(IBias(floor(end/2)+1:end), data.dcV(StartingIndex+floor(OneSweepLength/2):StartingIndex-1+OneSweepLength));
+        CriticalCurrent = GetCriticalCurrent(IBias(floor(end/2)+1:end), data.dcV(StartingIndex+floor(OneSweepLength/2):StartingIndex-1+OneSweepLength), SmoothSpan);
         %result.IcIndex(iCounter) = CriticalCurrent.minIndex;
         result.Ic(iCounter) = abs(CriticalCurrent.DiffMin);
         result.EndingIndex(iCounter) = k;

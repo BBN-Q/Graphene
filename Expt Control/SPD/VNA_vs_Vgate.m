ee4c2b81c436296bb05954604a78c36091722ab2
CoherenceController <buq-github@bbn.com>
2017-07-19 18:11:41 -0400

    VNA Measurement
    S21 Characterization

--- Expt Control/SPD/VNA_vs_Vgate.m
+++ Expt Control/SPD/VNA_vs_Vgate.m
@@ -7,17 +7,13 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [data] = VNA_R_vs_Vgate(VgateList, InitialWaitTime, measurementWaitTime)
+function [data] = VNA_vs_Vgate(VgateList, InitialWaitTime, measurementWaitTime)
 pause on;
 GateController = deviceDrivers.Keithley2400();
 GateController.connect('23');
-Lockin = deviceDrivers.SRS865();
-Lockin.connect('4');
 
 %%%%%%%%%%%%%%%%%%%%%       PLOT DATA     %%%%%%%%%%%%%%%%%%%%%%%%
 function plot_data()
-    figure(799); clf; plot(VgateList(1:k), data.X, '.-'); grid on;
-    xlabel('V_{bias} (V)'); ylabel('Lockin X (V)');
     figure(737); clf; imagesc(20*log10(abs(data.S)));
     %xlabel('V_{bias} (V)'); ylabel('Lockin X (V)');
 end
@@ -28,7 +24,7 @@ pause(InitialWaitTime);
 for k=1:length(VgateList)
     GateController.value = VgateList(k);
     pause(measurementWaitTime);
-    data.X(k) = Lockin.X; data.Y(k) = Lockin.Y;
+    % data.X(k) = Lockin.X; data.Y(k) = Lockin.Y;
     result = GetVNASpec_VNA();
     data.S(k,:) = result.S;
     plot_data()
@@ -36,8 +32,7 @@ end
 data.Freq = result.Freq;
 
 %%%%%%%%%%%%%%%%%%%%    BACK TO DEFAULT, CLEAN UP     %%%%%%%%%%%%%%%%%%%%%%%%%
-%Keithley.value = 0;
 GateController.value = 0;
 GateController.disconnect();
-pause off; clear result Lockin GateController;
+pause off; clear result GateController;
 end
